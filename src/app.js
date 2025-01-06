2815532bd4e16a6239605c91c69790ca4d2e30df
Sahil Singh <sahilsingh@MacBook-Pro.local>
2025-01-06 15:45:13 +0530

    added multer middleware for receiving files and cloudinary.js  in util for uplod on cloud

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 
 export {app}
